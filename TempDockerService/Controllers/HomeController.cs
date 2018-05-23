b427efdb30ca61c8636926cfac2e5ec8c21eea04
Soma Yarlagadda <soma.yarlagadda@vivint.com>
2018-05-23 11:05:38 -0600

    Test service for docker

---
+++ TempDockerService/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TempDockerService.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public string Index()
+        {
+            _logger.LogWarning("Inside the Health check");
+            _logger.LogError("Done processing");
+            return "I am up";
+        }
+    }
+}
---
+++ TempDockerService/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace TempDockerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                    options.Listen(IPAddress.Any, 44323, listenOptions =>
+                    {
+                        listenOptions.UseHttps("cert.pfx", "testPassword");
+                    });
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
